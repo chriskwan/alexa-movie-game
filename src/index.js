72ff49bd58782270318a577003b5cb6ac7439788
Chris Kwan <chriswkwan@gmail.com>
2016-04-30 14:49:08 -0400

    Add skateboard MVP for movie game

---
+++ src/index.js
@@ -0,0 +1,324 @@
+/**
+ Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+ http://aws.amazon.com/apache2.0/
+
+ or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+/**
+ * This sample shows how to create a simple Trivia skill with a multiple choice format. The skill
+ * supports 1 player at a time, and does not support games across sessions.
+ */
+
+'use strict';
+
+/**
+ * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+ * Make sure the first answer is the correct one. Set at least 4 answers, any extras will be shuffled in.
+ */
+var questions = [
+    {
+        movieName: "Frozen",
+        actors: [
+            "Kristen Bell",
+            "Idina Menzel",
+            "Josh Gad"
+        ]
+    },
+    {
+        movieName: "Transformers",
+        actors: [
+            "Shia LaBeouf",
+            "Megan Fox",
+            "Hugo Weaving"
+        ]
+    }
+];
+
+// Route the incoming request based on type (LaunchRequest, IntentRequest,
+// etc.) The JSON body of the request is provided in the event parameter.
+exports.handler = function (event, context) {
+    try {
+        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+
+//     if (event.session.application.applicationId !== "amzn1.echo-sdk-ams.app.05aecccb3-1461-48fb-a008-822ddrt6b516") {
+//         context.fail("Invalid Application ID");
+//      }
+
+        if (event.session.new) {
+            onSessionStarted({requestId: event.request.requestId}, event.session);
+        }
+
+        if (event.request.type === "LaunchRequest") {
+            onLaunch(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "IntentRequest") {
+            onIntent(event.request,
+                event.session,
+                function callback(sessionAttributes, speechletResponse) {
+                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === "SessionEndedRequest") {
+            onSessionEnded(event.request, event.session);
+            context.succeed();
+        }
+    } catch (e) {
+        context.fail("Exception: " + e);
+    }
+};
+
+/**
+ * Called when the session starts.
+ */
+function onSessionStarted(sessionStartedRequest, session) {
+    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // add any session init logic here
+}
+
+/**
+ * Called when the user invokes the skill without specifying what they want.
+ */
+function onLaunch(launchRequest, session, callback) {
+    console.log("onLaunch requestId=" + launchRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    getWelcomeResponse(callback);
+}
+
+/**
+ * Called when the user specifies an intent for this skill.
+ */
+function onIntent(intentRequest, session, callback) {
+    console.log("onIntent requestId=" + intentRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    var intent = intentRequest.intent,
+        intentName = intentRequest.intent.name;
+
+    // handle yes/no intent after the user has been prompted
+    if (session.attributes && session.attributes.userPromptedToContinue) {
+        delete session.attributes.userPromptedToContinue;
+        if ("AMAZON.NoIntent" === intentName) {
+            handleFinishSessionRequest(intent, session, callback);
+        } else if ("AMAZON.YesIntent" === intentName) {
+            handleRepeatRequest(intent, session, callback);
+        }
+    }
+
+    // dispatch custom intents to handlers here
+    if ("ActorAnswerIntent" === intentName) {
+        handleActorAnswerRequest(intent, session, callback);
+    } else if ("AMAZON.YesIntent" === intentName) {
+        handleActorAnswerRequest(intent, session, callback);
+    } else if ("AMAZON.NoIntent" === intentName) {
+        handleActorAnswerRequest(intent, session, callback);
+    } else if ("AMAZON.StartOverIntent" === intentName) {
+        getWelcomeResponse(callback);
+    } else if ("AMAZON.RepeatIntent" === intentName) {
+        handleRepeatRequest(intent, session, callback);
+    } else if ("AMAZON.HelpIntent" === intentName) {
+        handleGetHelpRequest(intent, session, callback);
+    } else if ("AMAZON.StopIntent" === intentName) {
+        handleFinishSessionRequest(intent, session, callback);
+    } else if ("AMAZON.CancelIntent" === intentName) {
+        handleFinishSessionRequest(intent, session, callback);
+    } else {
+        throw "Invalid intent";
+    }
+}
+
+/**
+ * Called when the user ends the session.
+ * Is not called when the skill returns shouldEndSession=true.
+ */
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
+        + ", sessionId=" + session.sessionId);
+
+    // Add any cleanup logic here
+}
+
+// ------- Skill specific business logic -------
+
+var GAME_LENGTH = questions.length;
+var CARD_TITLE = "Movie Game"; // Be sure to change this for your skill.
+
+function getWelcomeResponse(callback) {
+    var sessionAttributes = {},
+        speechOutput = "Movie Game. I will name a movie and you name an actor. Let's begin. ",
+        shouldEndSession = false,
+        currentQuestionIndex = 0,
+        spokenQuestion = questions[currentQuestionIndex].movieName,
+        repromptText = "Question 1. " + spokenQuestion + " ";
+
+    speechOutput += repromptText;
+    sessionAttributes = {
+        "speechOutput": repromptText,
+        "repromptText": repromptText,
+        "currentQuestionIndex": currentQuestionIndex,
+        "questions": questions,
+        "score": 0
+    };
+    callback(sessionAttributes,
+        buildSpeechletResponse(CARD_TITLE, speechOutput, repromptText, shouldEndSession));
+}
+
+function handleActorAnswerRequest(intent, session, callback) {
+    var speechOutput = "";
+    var sessionAttributes = {};
+    var gameInProgress = session.attributes && session.attributes.questions;
+    var answerSlotValid = true;
+    
+    if (!gameInProgress) {
+        // If the user responded with an answer but there is no game in progress, ask the user
+        // if they want to start a new game. Set a flag to track that we've prompted the user.
+        sessionAttributes.userPromptedToContinue = true;
+        speechOutput = "There is no game in progress. Do you want to start a new game? ";
+        callback(sessionAttributes,
+            buildSpeechletResponse(CARD_TITLE, speechOutput, speechOutput, false));
+    } else {
+        var gameQuestions = session.attributes.questions,
+            currentScore = parseInt(session.attributes.score),
+            currentQuestionIndex = parseInt(session.attributes.currentQuestionIndex),
+            possibleCorrectAnswers = questions[session.attributes.currentQuestionIndex].actors
+
+        var speechOutputAnalysis = "";
+
+        var inPossibleAnswers = false;
+        for (var i=0; i<possibleCorrectAnswers.length; i++) {
+            if (possibleCorrectAnswers[i].toLowerCase() === intent.slots.ActorName.value.toLowerCase()) {
+                inPossibleAnswers = true;
+                break;
+            }
+        }
+        if (inPossibleAnswers) {
+            currentScore++;
+            speechOutputAnalysis = "correct. ";
+        } else {
+            speechOutputAnalysis = "wrong. "
+            speechOutputAnalysis += "The correct answer is " + possibleCorrectAnswers.join(" or ") + ". ";
+        }
+        // if currentQuestionIndex is 4, we've reached 5 questions (zero-indexed) and can exit the game session
+        if (currentQuestionIndex == GAME_LENGTH - 1) {
+            speechOutput += speechOutputAnalysis + "You got " + currentScore.toString() + " out of "
+                + GAME_LENGTH.toString() + " questions correct. Thank you for playing!";
+            callback(session.attributes,
+                buildSpeechletResponse(CARD_TITLE, speechOutput, "", true));
+        } else {
+            currentQuestionIndex += 1;
+            var spokenQuestion = questions[currentQuestionIndex].movieName;
+            var questionIndexForSpeech = currentQuestionIndex + 1,
+                repromptText = "Question " + questionIndexForSpeech.toString() + ". " + spokenQuestion + " ";
+
+            speechOutput += speechOutputAnalysis + "Your score is " + currentScore.toString() + ". " + repromptText;
+
+            sessionAttributes = {
+                "speechOutput": repromptText,
+                "repromptText": repromptText,
+                "currentQuestionIndex": currentQuestionIndex,
+                "questions": questions,
+                "score": currentScore
+            };
+            callback(sessionAttributes,
+                buildSpeechletResponse(CARD_TITLE, speechOutput, repromptText, false));
+        }
+    }
+}
+
+function handleRepeatRequest(intent, session, callback) {
+    // Repeat the previous speechOutput and repromptText from the session attributes if available
+    // else start a new game session
+    if (!session.attributes || !session.attributes.speechOutput) {
+        getWelcomeResponse(callback);
+    } else {
+        callback(session.attributes,
+            buildSpeechletResponseWithoutCard(session.attributes.speechOutput, session.attributes.repromptText, false));
+    }
+}
+
+function handleGetHelpRequest(intent, session, callback) {
+    // Provide a help prompt for the user, explaining how the game is played. Then, continue the game
+    // if there is one in progress, or provide the option to start another one.
+
+    // Set a flag to track that we're in the Help state.
+    session.attributes.userPromptedToContinue = true;
+
+    // Do not edit the help dialogue. This has been created by the Alexa team to demonstrate best practices.
+
+    var speechOutput = "I will ask you " + GAME_LENGTH + " multiple choice questions. Respond with the number of the answer. "
+        + "For example, say one, two, three, or four. To start a new game at any time, say, start game. "
+        + "To repeat the last question, say, repeat. "
+        + "Would you like to keep playing?",
+        repromptText = "To give an answer to a question, respond with the number of the answer . "
+        + "Would you like to keep playing?";
+        var shouldEndSession = false;
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard(speechOutput, repromptText, shouldEndSession));
+}
+
+function handleFinishSessionRequest(intent, session, callback) {
+    // End the session with a "Good bye!" if the user wants to quit the game
+    callback(session.attributes,
+        buildSpeechletResponseWithoutCard("Good bye!", "", true));
+}
+
+// ------- Helper functions to build responses -------
+
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        card: {
+            type: "Simple",
+            title: title,
+            content: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
+    return {
+        outputSpeech: {
+            type: "PlainText",
+            text: output
+        },
+        reprompt: {
+            outputSpeech: {
+                type: "PlainText",
+                text: repromptText
+            }
+        },
+        shouldEndSession: shouldEndSession
+    };
+}
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    };
+}
